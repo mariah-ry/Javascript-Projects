3ea958599ba0339c238c26eb7d1da5856a23fd27
mariah-ry <77254418+mariah-ry@users.noreply.github.com>
2021-02-21 20:04:23 -0800

    Creating main.js

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var student1 = "Jenni", student2 = "Lisa", student3 = "Jisoo", student4 = "Rose"; //Assigning student variables
+var student1 = student1.fontcolor("red"), student2 = student2.fontcolor("blue"), student3 = student3.fontcolor("purple"), student4 = student4.fontcolor("Pink"); //Assigning font colors to each variable
+
+
+var A = "10"; //assigning the value 10 to variable A
+var B = "5"; //assigning value 5 to B
+var C = A + B;
+
+window.alert("Hello, world!")
+document.write(C)
